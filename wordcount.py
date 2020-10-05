09f0af011c4bd32990c850517584602e789d26e2
sunniraleigh <sraleigh@smith.edu>
2020-10-05 19:49:55 +0000

    write and test count_word_occurence, delete orig pseudo code

--- wordcount.py
+++ wordcount.py
@@ -1,7 +1,19 @@
-def count_word_occurence(file):
+def count_word_occurence(file_path):
     """ Counts and prints the occurence of each word in a file."""
     # open a file
-    # read each word in a file into a list
-    # deliminator should be a single space
-    # iterate over each item in a list
-    # each unique word becomes a key in the dictionary and its occurence becomes its value
+    file = open(file_path)
+
+    # define empty dictionary
+    word_counts = {}
+
+    for line in file:
+        line = line.strip() # strip white space
+        words = line.split(" ") # retreive individual words from a line
+        for word in words:
+            word_counts[word] = word_counts.get(word, 0) + 1 # add each unique word to word_counts and count them
+    
+    for word in word_counts:
+        print(word, word_counts[word]) # print each word and its occurence
+
+#count_word_occurence("test.txt")
+#count_word_occurence("twain.txt")

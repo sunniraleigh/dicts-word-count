4d029535f2cf616542573eec822e50b759032a03
sunniraleigh <sraleigh@smith.edu>
2020-10-05 19:35:04 +0000

    write pseudo code for function

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,7 @@
-# put your code here.
+def count_word_occurence(file):
+    """ Counts and prints the occurence of each word in a file."""
+    # open a file
+    # read each word in a file into a list
+    # deliminator should be a single space
+    # iterate over each item in a list
+    # each unique word becomes a key in the dictionary and its occurence becomes its value
